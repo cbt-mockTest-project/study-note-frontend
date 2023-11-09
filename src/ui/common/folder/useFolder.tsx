9e0aec06fec1372e95e374518dec9d722042a00e
eungwang <scm323@naver.com>
2023-11-09 22:37:02 +0900

    Feat: useFolder hooks 작성

---
+++ src/ui/common/folder/useFolder.tsx
@@ -0,0 +1,46 @@
+import { CreateFolderInput, createFolder, getFolders } from "@/lib/apis/folder";
+import { FolderFilter } from "@/types/folder";
+import { message } from "antd";
+import { cloneDeep } from "lodash";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
+import useSWR from "swr";
+
+const useFolder = () => {
+  const searchParams = useSearchParams();
+  const filter = searchParams.get("filter") as FolderFilter;
+
+  const {
+    data: getFoldersResponse,
+    mutate: mutateFolders,
+    isLoading: isLoadingFolders,
+  } = useSWR(["/folder", filter], () => getFolders(filter));
+
+  const folders = useMemo(
+    () => getFoldersResponse?.data.folders || [],
+    [getFoldersResponse]
+  );
+
+  const handleCreateFolder = async (createFolderInput: CreateFolderInput) => {
+    try {
+      const { data } = await createFolder(createFolderInput);
+      if (data.ok) {
+        const copiedResponse = cloneDeep(getFoldersResponse);
+        if (!copiedResponse?.data.folders) return;
+        copiedResponse.data.folders.unshift(data.folder);
+        return mutateFolders(copiedResponse, false);
+      }
+      message.error(data.error);
+    } catch (e) {
+      message.error("폴더를 생성할 수 없습니다.");
+      console.log(e);
+    }
+  };
+  return {
+    handleCreateFolder,
+    folders,
+    isLoadingFolders,
+  };
+};
+
+export default useFolder;
