0789ceac29a94f6b173ee567efea5a67a9de9de5
eungwang1 <scm323@naver.com>
2023-11-14 10:14:02 +0900

    Feat: svgr webpack 설정

--- next.config.js
+++ next.config.js
@@ -4,8 +4,27 @@ const nextConfig = {
         styledComponents: true,
     },
     images: {
-        domains: ['lh3.googleusercontent.com', 'modu-study-note.s3.ap-northeast-2.amazonaws.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname:  '**'
+            }, 
+            {
+                protocol: 'https',
+                hostname: 'modu-study-note.s3.ap-northeast-2.amazonaws.com',
+                pathname:  '**'
+            }
+        ]
     },
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+          });
+          return config;
+    }
 }
 
 module.exports = nextConfig
