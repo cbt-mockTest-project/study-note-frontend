ea38de6a36f743a190ddf6b60a8b55c96aa5a5d4
eungwang <scm323@naver.com>
2023-11-18 01:16:25 +0900

    Feat: study-note-item 컴포넌트 분리및 체크박스 UI추가

---
+++ src/ui/folder/StudyNoteItem.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import styled from "styled-components";
+import Card from "../common/card/Card";
+import { IStudyNote } from "@/types/studyNote";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { colors } from "@/styles/colors";
+import { Checkbox } from "antd";
+import Link from "next/link";
+import { breakpoint } from "@/lib/responsive";
+
+const StudyNoteItemBlock = styled.div`
+  display: flex;
+  width: calc(45% - 5px);
+  gap: 15px;
+  .study-note-link {
+    width: 100%;
+  }
+  .study-note-card-inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+  .study-note-title-count-wrapper {
+    display: flex;
+    gap: 5px;
+    flex-direction: column;
+  }
+  .study-note-count {
+    font-size: 12px;
+    color: ${colors.gray_400};
+    font-weight: bold;
+  }
+  .study-note-title {
+    font-weight: bold;
+    color: ${colors.gray_700};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+  }
+
+  .study-note-delete-button {
+    transition: background-color 0.2s ease-in-out;
+    border-radius: 50%;
+    padding: 5px;
+    margin: 0;
+    &:hover {
+      background-color: ${colors.gray_300};
+    }
+  }
+
+  @media (max-width: ${breakpoint.md}) {
+    width: 100%;
+  }
+`;
+
+interface StudyNoteItemProps {
+  studyNote: IStudyNote;
+  folderId: number;
+  removeNoteFromFolder: (studyNoteId: number) => void;
+}
+
+const StudyNoteItem: React.FC<StudyNoteItemProps> = ({
+  studyNote,
+  removeNoteFromFolder,
+  folderId,
+}) => {
+  return (
+    <StudyNoteItemBlock>
+      <Checkbox />
+      <Link
+        className="study-note-link"
+        href={`/study-note/create?fid=${folderId}&nid=${studyNote.id}`}
+      >
+        <Card className="study-note-card">
+          <div className="study-note-card-inner">
+            <div className="study-note-title-count-wrapper">
+              <p className="study-note-title">{studyNote.name}</p>
+              <p className="study-note-count">
+                {studyNote.studyCardOrder.length}문항
+              </p>
+            </div>
+            <button
+              className="study-note-delete-button"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                removeNoteFromFolder(studyNote.id);
+              }}
+            >
+              <DeleteIcon />
+            </button>
+          </div>
+        </Card>
+      </Link>
+    </StudyNoteItemBlock>
+  );
+};
+
+export default StudyNoteItem;
--- src/ui/folder/StudyNoteList.tsx
+++ src/ui/folder/StudyNoteList.tsx
@@ -1,57 +1,15 @@
 import { IStudyNote } from "@/types/studyNote";
 import React from "react";
 import styled from "styled-components";
-import Card from "../common/card/Card";
-import { colors } from "@/styles/colors";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { breakpoint } from "@/lib/responsive";
-import Link from "next/link";
-import { IFolder } from "@/types/folder";
 import { removeStudyNoteFromFolderAPI } from "@/lib/apis/studyNote";
 import { message } from "antd";
+import StudyNoteItem from "./StudyNoteItem";
 
 const StudyNoteListBlock = styled.ul`
   margin-top: 20px;
   display: flex;
-  gap: 10px;
+  gap: 10px 50px;
   flex-wrap: wrap;
-  .study-note-link {
-    width: calc(50% - 5px);
-  }
-
-  .study-note-title {
-    font-weight: bold;
-    color: ${colors.gray_700};
-  }
-  .study-note-card-inner {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .study-note-delete-button {
-    transition: background-color 0.2s ease-in-out;
-    border-radius: 50%;
-    padding: 5px;
-    margin: 0;
-    &:hover {
-      background-color: ${colors.gray_300};
-    }
-  }
-  .study-note-title-count-wrapper {
-    display: flex;
-    gap: 5px;
-    flex-direction: column;
-  }
-  .study-note-count {
-    font-size: 12px;
-    color: ${colors.gray_400};
-    font-weight: bold;
-  }
-  @media (max-width: ${breakpoint.md}) {
-    .study-note-card {
-      width: 100%;
-    }
-  }
 `;
 
 interface StudyNoteListProps {
@@ -78,32 +36,12 @@ const StudyNoteList: React.FC<StudyNoteListProps> = ({
   return (
     <StudyNoteListBlock>
       {studyNotes?.map((studyNote) => (
-        <Link
-          className="study-note-link"
-          href={`/study-note/create?fid=${folderId}&nid=${studyNote.id}`}
+        <StudyNoteItem
           key={studyNote.id}
-        >
-          <Card className="study-note-card" key={studyNote.id}>
-            <div className="study-note-card-inner">
-              <div className="study-note-title-count-wrapper">
-                <p className="study-note-title">{studyNote.name}</p>
-                <p className="study-note-count">
-                  {studyNote.studyCardOrder.length}문항
-                </p>
-              </div>
-              <button
-                className="study-note-delete-button"
-                onClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  removeNoteFromFolder(studyNote.id);
-                }}
-              >
-                <DeleteIcon />
-              </button>
-            </div>
-          </Card>
-        </Link>
+          folderId={folderId}
+          studyNote={studyNote}
+          removeNoteFromFolder={removeNoteFromFolder}
+        />
       ))}
     </StudyNoteListBlock>
   );
